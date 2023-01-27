a46fa794ca91fb9e4281d27fd53c8056c4aecea1
Jeremy Theintz <jeremy@appogee.ch>
2023-01-27 17:50:20 +0100

    🚀 init frontend application

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Node Guardians</title>
+				<meta name='description' content='Node Guardians frontend' />
+			</Head>
+
+			<main></main>
+		</>
+	);
+}
---
+++ styles/GlobalStyles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle, DefaultTheme } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
+//========================================================================================================
+// GENERAL
+//========================================================================================================
+* {
+	box-sizing: border-box;
+}
+*::before {
+	box-sizing: border-box;
+}
+*::after {
+	box-sizing: border-box;
+}
+
+body {
+  background-color: ${({ theme }) => theme.colors.black};
+}
+
+`;
+
+export default GlobalStyle;
